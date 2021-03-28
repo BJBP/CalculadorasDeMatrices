2dc108a83ec637e1b328c3c718a026b52e638912
jaison_03042000 <jaison_03042000@hotmail.com>
2021-03-28 16:43:17 -0500

    a

---
+++ Otros/2files/src/pkg2files/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2files;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+/**
+ *
+ * @author BJBP
+ */
+public class Main  extends javax.swing.JFrame implements ActionListener{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        a b=new a();
+         b.B();
+         //Formulario c=new Formulario();
+         AA.main1();
+         
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
